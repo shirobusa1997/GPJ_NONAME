ee51856a270ce6dfcf2c887671e0fc34848811c5
Mojatto_WS <daichiandroid1997@gmail.com>
2020-03-20 00:01:43 +0900

    アセットを追加．

--- Source/GPJ_NONAME/GPJ_NONAME.Build.cs
+++ Source/GPJ_NONAME/GPJ_NONAME.Build.cs
@@ -8,9 +8,9 @@ public class GPJ_NONAME : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "imgui" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "imgui" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
