57a0f1fb2604719762db3b8b65817184b152f27b
Mojatto <daichiandroid1997@gmail.com>
2020-05-20 14:17:28 +0900

    KawaiiPhysicsプラグインを追加．

---
+++ Plugins/KawaiiPhysics-master/Plugins/KawaiiPhysics/Source/KawaiiPhysics/Public/AnimNode_KawaiiPhysics.h
@@ -0,0 +1,310 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Animation/AnimNodeBase.h"
+#include "Animation/AnimNodeSpaceConversions.h"
+#include "BoneControllers/AnimNode_ModifyBone.h"
+#include "BoneControllers/AnimNode_AnimDynamics.h"
+
+#include "BoneContainer.h"
+#include "BonePose.h"
+#include "BoneControllers/AnimNode_SkeletalControlBase.h"
+
+#include "AnimNode_KawaiiPhysics.generated.h"
+
+UENUM()
+enum class EPlanarConstraint : uint8
+{
+	None,
+	X,
+	Y,
+	Z,
+};
+
+USTRUCT()
+struct FCollisionLimitBase
+{
+	GENERATED_BODY();
+
+	/** Bone to attach the sphere to */
+	UPROPERTY(EditAnywhere, Category = CollisionLimitBase)
+	FBoneReference DrivingBone;
+
+	UPROPERTY(EditAnywhere, Category = CollisionLimitBase)
+	FVector OffsetLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, Category = CollisionLimitBase, meta = (ClampMin = "-360", ClampMax = "360"))
+	FRotator OffsetRotation = FRotator::ZeroRotator;
+
+	UPROPERTY()
+	FVector Location = FVector::ZeroVector;
+
+	UPROPERTY()
+	FQuat Rotation = FQuat::Identity;
+
+};
+
+USTRUCT()
+struct FSphericalLimit : public FCollisionLimitBase
+{
+	GENERATED_BODY();
+
+	/** Radius of the sphere */
+	UPROPERTY(EditAnywhere, Category = SphericalLimit, meta = (ClampMin = "0"))
+	float Radius = 5.0f;
+
+	/** Whether to lock bodies inside or outside of the sphere */
+	UPROPERTY(EditAnywhere, Category = SphericalLimit)
+	ESphericalLimitType LimitType = ESphericalLimitType::Outer;
+};
+
+USTRUCT()
+struct FCapsuleLimit : public FCollisionLimitBase
+{
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, Category = CapsuleLimit, meta = (ClampMin = "0"))
+	float Radius = 5.0f;
+
+	UPROPERTY(EditAnywhere, Category = CapsuleLimit, meta = (ClampMin = "0"))
+	float Length = 10.0f;
+};
+
+USTRUCT()
+struct FPlanarLimit : public FCollisionLimitBase
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+	FPlane Plane;
+};
+
+USTRUCT(BlueprintType)
+struct KAWAIIPHYSICS_API FKawaiiPhysicsSettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0"), category = "KawaiiPhysics")
+	float Damping = 0.1f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0"), category = "KawaiiPhysics")
+	float WorldDampingLocation = 0.8f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0"), category = "KawaiiPhysics")
+	float WorldDampingRotation = 0.8f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0"), category = "KawaiiPhysics")
+	float Stiffness = 0.05f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0"), category = "KawaiiPhysics")
+	float Radius = 3.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (PinHiddenByDefault, ClampMin = "0"), category = "KawaiiPhysics")
+	float LimitAngle = 0.0f;
+};
+
+USTRUCT()
+struct KAWAIIPHYSICS_API FKawaiiPhysicsModifyBone
+{
+	GENERATED_USTRUCT_BODY()
+	
+public:
+	UPROPERTY()
+	FBoneReference BoneRef;
+	UPROPERTY()
+	int ParentIndex = -1;
+	UPROPERTY()
+	TArray<int> ChildIndexs;
+
+	UPROPERTY()
+	FKawaiiPhysicsSettings PhysicsSettings;
+
+	UPROPERTY()
+	FVector Location;
+	UPROPERTY()
+	FVector PrevLocation;
+	UPROPERTY()
+	FQuat PrevRotation;
+	UPROPERTY()
+	FVector PoseLocation;
+	UPROPERTY()
+	FQuat PoseRotation;
+	UPROPERTY()
+	FVector PoseScale;
+	UPROPERTY()
+	float LengthFromRoot;
+	UPROPERTY()
+	bool bDummy = false;
+
+
+public:
+
+	void UpdatePoseTranform(const FBoneContainer& BoneContainer, FCSPose<FCompactPose>& Pose)
+	{
+		auto CompactPoseIndex = BoneRef.GetCompactPoseIndex(BoneContainer);
+		if (CompactPoseIndex < 0)
+		{
+			PoseLocation = FVector::ZeroVector;
+			PoseRotation = FQuat::Identity;
+			PoseScale = FVector::OneVector;
+			return;
+		}
+
+		auto ComponentSpaceTransform = Pose.GetComponentSpaceTransform(CompactPoseIndex);
+		PoseLocation = ComponentSpaceTransform.GetLocation();
+		PoseRotation = ComponentSpaceTransform.GetRotation();
+		PoseScale = ComponentSpaceTransform.GetScale3D();
+	}
+};
+
+USTRUCT(BlueprintType)
+struct KAWAIIPHYSICS_API FAnimNode_KawaiiPhysics : public FAnimNode_SkeletalControlBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = ModifyTarget)
+	FBoneReference RootBone;
+	UPROPERTY(EditAnywhere, Category = ModifyTarget)
+	TArray<FBoneReference> ExcludeBones;
+
+	UPROPERTY(EditAnywhere, Category = TargetFramerate, meta = (EditCondition = "OverrideTargetFramerate"))
+	int TargetFramerate = 60;
+	UPROPERTY(EditAnywhere, Category = TargetFramerate, meta = (InlineEditConditionToggle))
+	bool OverrideTargetFramerate = false;
+
+	/** Settings for control of physical behavior */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	FKawaiiPhysicsSettings PhysicsSettings;
+	
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* DampingCurve = nullptr;
+
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* WorldDampingLocationCurve = nullptr;
+
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* WorldDampingRotationCurve = nullptr;
+
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* StiffnessCurve = nullptr;
+
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* RadiusCurve = nullptr;
+
+	/** Curve for adjusting the set value of physical behavior. Use rate of bone length from Root */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Settings", meta = (PinHiddenByDefault))
+	UCurveFloat* LimitAngleCurve = nullptr;
+
+	/** Flag to update each frame physical parameter. Disable to improve performance */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Advanced Physics Settings", meta = (PinHiddenByDefault))
+	bool bUpdatePhysicsSettingsInGame = true;
+	
+	/** Add a dummy bone to the end bone if it's above 0. It affects end bone rotation. For example, it rotates well if it is short */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Advanced Physics Settings", meta = (PinHiddenByDefault, ClampMin = "0"))
+	float DummyBoneLength = 0.0f;
+
+	/** Fix the bone on the specified plane  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Advanced Physics Settings", meta = (PinHiddenByDefault))
+	EPlanarConstraint PlanarConstraint = EPlanarConstraint::None;
+
+
+	UPROPERTY(EditAnywhere, Category = "Spherical Limits")
+	TArray< FSphericalLimit> SphericalLimits;
+	UPROPERTY(EditAnywhere, Category = "Capsule Limits")
+	TArray< FCapsuleLimit> CapsuleLimits;
+	UPROPERTY(EditAnywhere, Category = "Planar Limits")
+	TArray< FPlanarLimit> PlanarLimits;
+
+	/** If the movement amount of one frame exceeds the threshold, ignore the movement  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport", meta = (PinHiddenByDefault))
+	float TeleportDistanceThreshold = 300.0f;
+
+	/** If the rotation amount of one frame exceeds the threshold, ignore the rotation  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport", meta = (PinHiddenByDefault))
+	float TeleportRotationThreshold = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ExternalForce", meta = (PinHiddenByDefault))
+	FVector Gravity = FVector::ZeroVector;
+	
+	/** Whether or not wind is enabled for the bodies in this simulation */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Wind, meta = (PinHiddenByDefault))
+	bool bEnableWind = false;
+
+	/** Scale to apply to calculated wind velocities in the solver */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Wind, meta = (DisplayAfter = "bEnableWind"), meta = (PinHiddenByDefault))
+	float WindScale = 1.0f;
+
+
+	UPROPERTY()
+	TArray< FKawaiiPhysicsModifyBone > ModifyBones;
+
+
+private:
+	UPROPERTY()
+	float TotalBoneLength = 0;
+	UPROPERTY()
+	FTransform PreSkelCompTransform;
+	UPROPERTY()
+	bool bInitPhysicsSettings = false;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY()
+	bool bEditing = false;
+#endif
+
+	FVector SkelCompMoveVector;
+	FQuat SkelCompMoveRotation;
+	float DeltaTime;
+	float DeltaTimeOld;
+
+public:
+	FAnimNode_KawaiiPhysics();
+
+	// FAnimNode_Base interface
+	//virtual void GatherDebugData(FNodeDebugData& DebugData) override;
+	virtual void Initialize_AnyThread(const FAnimationInitializeContext& Context) override;
+	virtual void CacheBones_AnyThread(const FAnimationCacheBonesContext& Context) override;
+	// End of FAnimNode_Base interface
+
+	// FAnimNode_SkeletalControlBase interface
+	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) override;
+	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
+	virtual void UpdateInternal(const FAnimationUpdateContext& Context) override;
+	// End of FAnimNode_SkeletalControlBase interface
+
+	// 
+	void InitModifyBones(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer);
+	float GetTotalBoneLength() 
+	{
+		return TotalBoneLength;
+	}
+
+private:
+	// FAnimNode_SkeletalControlBase interface
+	virtual void InitializeBoneReferences(const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
+
+	int AddModifyBone(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, const FReferenceSkeleton& RefSkeleton, int BoneIndex);
+	
+	// clone from FReferenceSkeleton::GetDirectChildBones
+	int32 CollectChildBones(const FReferenceSkeleton& RefSkeleton, int32 ParentBoneIndex, TArray<int32> & Children) const;
+	void CalcBoneLength(FKawaiiPhysicsModifyBone& Bone, const TArray<FTransform>& RefBonePose);
+
+	void UpdatePhysicsSettingsOfModifyBones();
+	void UpdateSphericalLimits(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, FTransform& ComponentTransform);
+	void UpdateCapsuleLimits(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, FTransform& ComponentTransform);
+	void UpdatePlanerLimits(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, FTransform& ComponentTransform);
+
+	void SimulateModfyBones(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, FTransform& ComponentTransform);
+	void AdjustBySphereCollision(FKawaiiPhysicsModifyBone& Bone);
+	void AdjustByCapsuleCollision(FKawaiiPhysicsModifyBone& Bone);
+	void AdjustByPlanerCollision(FKawaiiPhysicsModifyBone& Bone);
+	void AdjustByAngleLimit(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, FTransform& ComponentTransform, FKawaiiPhysicsModifyBone& Bone, FKawaiiPhysicsModifyBone& ParentBone);
+	void AdjustByPlanarConstraint(FKawaiiPhysicsModifyBone& Bone, FKawaiiPhysicsModifyBone& ParentBone);
+	
+
+	void ApplySimuateResult(FComponentSpacePoseContext& Output, const FBoneContainer& BoneContainer, TArray<FBoneTransform>& OutBoneTransforms);
+	
+};
---
+++ Plugins/KawaiiPhysics-master/README.md
@@ -0,0 +1,109 @@
+# Kawaii Physics
+English doc :   
+https://github.com/pafuhana1213/KawaiiPhysics/blob/master/README_en.md  
+
+Forum :   
+日本語  https://forums.unrealengine.com/international/japan/1679269  
+英語  https://forums.unrealengine.com/community/released-projects/1638095
+
+## はじめに
+Kawaii Physicsは UnrealEngine4用に作成した疑似物理プラグインです。  
+髪、スカート、胸などの揺れものを「かんたんに」「かわいく」揺らすことができます。
+
+![](https://github.com/pafuhana1213/Screenshot/blob/master/KawaiiPhysics1.gif)  
+お借りしたキャラクタ：Gray ちゃん http://rarihoma.xvs.jp/products/graychan
+
+![](https://github.com/pafuhana1213/Screenshot/blob/master/KawaiiPhysics0.gif)  
+https://www.youtube.com/watch?v=UvpEIBGegvs  
+お借りしたキャラクタ：ミライ小町 https://www.bandainamcostudios.com/works/miraikomachi/
+
+## 特徴
+![](https://github.com/pafuhana1213/Screenshot/blob/master/KawaiiPhysics2.jpg)  
+![](https://github.com/pafuhana1213/Screenshot/blob/master/KawaiiPhysics4.gif)  
+- 元の形状を尊重しつつ、アニメーションやSkeletalMeshComponentの移動・回転を元に物理制御を行います。
+- プラグインに含まれる「KawaiiPhysicsノード」をAnimationBPのAnimGraphで使う形です。
+- 指定したボーンとそれ以下のボーンをキャラクタの動きに合わせて揺らせます。
+- 物理制御用のパラメータは2種類だけなので、エンジン標準の[AnimDynamics](https://docs.unrealengine.com/ja/Engine/Animation/NodeReference/SkeletalControls/AnimDynamics/index.html)に比べて簡単にセットアップできます。
+- 球・カプセル・平面コリジョンを追加することができます
+- アニメーションエディタのビューポート上で各コリジョンの位置・向き・大きさを調整できます
+- 骨の長さを維持するため、仮に計算が破綻しても骨が伸び縮みすることがありません。
+- PhysXは使わずにシンプルなアルゴリズムを使用しているため、エンジン標準の物理システムに比べて負荷が低い（はず）です。
+
+物理挙動を実装するにあたって参考にした資料  
+[次期アイドルマスター グラフィクス＆アニメーション プログラミング プレビュー](https://cedil.cesa.or.jp/cedil_sessions/view/416)
+
+## 動作環境
+- UE4.24.2  
+
+UE4.21以前でビルドする場合は、KawaiiPhysicsEditMode.cpp における  
+GEngine->ConstraintLimitMaterialPrismatic->GetRenderProxy () を  
+GEngine->ConstraintLimitMaterialPrismatic->GetRenderProxy (false)　に修正する必要があります。 
+
+- UE4.20~4.23（Pluginのみ）  
+https://github.com/pafuhana1213/KawaiiPhysics/releases/
+
+## 使い方
+- プロジェクトのPluginsフォルダにKawaiiPhysicsフォルダを入れてください
+- 各パラメータについて：https://github.com/pafuhana1213/KawaiiPhysics/wiki/%E5%90%84%E3%83%91%E3%83%A9%E3%83%A1%E3%83%BC%E3%82%BF%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
+
+## サンプル
+![](https://github.com/pafuhana1213/Screenshot/blob/master/KawaiiPhysics3.jpg)  
+- Content/KawaiiPhysicsSample/KawaiiPhysicsSample
+- お借りしたキャラクタ：Gray ちゃん http://rarihoma.xvs.jp/products/graychan
+
+## 内部実装について
+その1 http://pafuhana1213.hatenablog.com/entry/2019/07/26/171046
+
+## ライセンス
+MIT
+
+## 採用実績
+https://github.com/pafuhana1213/KawaiiPhysics/wiki/%E6%8E%A1%E7%94%A8%E5%AE%9F%E7%B8%BE-Adoption
+
+## 作者
+[おかず@pafuhana1213](https://twitter.com/pafuhana1213)
+
+## ハッシュタグ
+[#KawaiiPhysics](https://twitter.com/search?q=%23kawaiiphysics&src=typed_query&f=live)
+
+## 履歴
+2020/3/13 v1.4.4  
+MacOSでビルドに失敗する不具合の修正  
+(melMass 様、ありがとうございました！)  
+2020/3/13 v1.4.3  
+Package作成に失敗する不具合を修正  
+2020/2/16 v1.4.2  
+Standalone実行時に正常に動作しない不具合を修正  
+2020/1/31 v1.4.1  
+**SphereLimitのLimitTypeがInnerの場合、Outer設定時に異なりBoneのRadius設定を考慮してなかった不具合の修正  
+1.3.1以前の挙動に戻したい方は CVarEnableOldPhysicsMethodSphereLimit を 1 に設定してください**    
+2020/1/31 v1.4.0  
+**重力変数の位置更新の計算がgt^2でなくgtになっていたので修正  
+1.3.1以前の重力計算を使用したい場合は　p.KawaiiPhysics.EnableOldPhysicsMethodGravity を 1 に設定してください**  
+2020/1/31 v1.3.1  
+TeleportRotationThresholdの計算にてラジアンと度が混在していたことでWorldRotationのテレポート対応がほぼ機能してなかった不具合を修正  
+(monguri 様、ありがとうございました！)  
+2020/1/31 v1.3.0  
+UE4.24.2 に対応  
+(TheHoodieGuy02 様、ありがとうございました！)  
+2019/10/29 v1.2.1  
+・World Damping系を従来処理に差し戻し。30fps時の挙動が更に安定しました  
+(@seiko_dev 様、ありがとうございました！)  
+2019/10/26 v1.2.0  
+・簡易可変フレームレート対応。フレームレート低下時の挙動が少し安定するようになったはず  
+(@seiko_dev 様、ありがとうございました！)  
+・基準フレームレートを設定できるようにしました(デフォルト：60)（正直テスト不足です。何かあったらissueへ  
+・カーブの調整用に各ボーンの始点からの長さの割合をデバッグ表示するようにしました  
+2019/10/19 v1.1.2   
+BoneのScaleが(1,1,1)以外の環境に対応できてなかったので修正  
+(shop-0761様、ありがとうございました！)  
+2019/9/11 v1.1.1  
+Bone.LengthFromRoot の計算が間違っていたのを修正  
+(KazumasaOhashi様、ありがとうございました！)  
+2019/8/26 v1.1   
+WindDirectionalSourceに対応しました(注意：従来のWind設定に対して破壊的変更が入ります)  
+2019/7/20 v1.0.1   
+CollisionのOffsetLocationがボーンのRotationを考慮していない不具合の修正  
+(v1.0 におけるOffsetLocationの挙動が大きく変化します)  
+2019/7/2 v1.0 公開 v1.0
+
